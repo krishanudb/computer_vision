aed92b1215eb53d81d7682430a7be020b30234d1
Krishanu <krish.dblogger@gmail.com>
2019-09-13 13:23:55 +0530

    fehem bugs solved

--- assignment_2/fehem.py
+++ assignment_2/fehem.py
@@ -1,6 +1,6 @@
 # This code is for feature extraction, homography estimation and matching. 
 # The strategy used is described in steps below:
-# Step 1: Extract the features using SIFT
+# Step 1: Extract the features using SIFT and find unique features using the method previously described.
 # Step 2: Match the features using FLANN (this is fastest for large datasets)
 # Step 3: Store the best matches using Lowe's ratio test (gievn in SIFT Paper), which will then be considered for homography estimation
 # (Note: The Image pairs with best matches greater than 300 will be considered for panorma construction 
@@ -17,6 +17,9 @@ import sys
 import os
 import matplotlib.pyplot as plt
 import cv2
+
+from sklearn.cluster import MiniBatchKMeans
+
 print('OpenCV 3.4.0.12 required. Install this, if current version is different')
 print('Current OpenCV version: ',cv2.__version__)
 
@@ -29,21 +32,38 @@ kpics = {}
 kp = {}
 des = {}
 
+nkp = {}
+ndes = {}
+
 for filee in files:
     pics[filee.split(".")[0]] = cv2.imread(folder + "/" + filee,0)
     kpics[filee.split(".")[0]] = cv2.imread(folder + "/" + filee,0)
 print("All Images Read.")
 
 for key, value in kpics.items():
-    print("Extracting features for Image {}".format(key))
+    print("Extracting all features for Image {}".format(key))
     sift = cv2.xfeatures2d.SIFT_create()
     keypoints, descriptors = sift.detectAndCompute(kpics[key], None)
     kp[key] = keypoints
     des[key] = descriptors
-print("Finding features done.")
+    print("Finding unique features from Image {}".format(key))
+    kmeans = MiniBatchKMeans(n_clusters=200, random_state=0, batch_size = len(kp[key]) / 100).fit(des[key])
+
+    clusters = kmeans.labels_
+    distances = np.sqrt(np.sum((des[key] - kmeans.cluster_centers_[clusters]) ** 2, axis = 1))
+
+    percentile_90 = np.percentile(distances, 90)
+
+    ndes[key] = des[key][distances > percentile_90]
+    nkp[key] = list(np.array(kp[key])[distances > percentile_90])
+    
+des = ndes
+kp = nkp
+
+print("Finding unique features done.")
 
 print("Feature matching among all pairs of Images starting.")
-good_matches = []
+matches_dict = {}
 for i in kpics.keys():
     matches_dict[i] = {}
     for j in kpics.keys():
@@ -55,7 +75,8 @@ for i in kpics.keys():
             search_params = dict(checks=50)   # or pass empty dictionary
             flann = cv2.FlannBasedMatcher(index_params,search_params)
             matches = flann.knnMatch(des[key1],des[key2],k=2)
-            matchesMask = [[0,0] for i in range(len(matches))]
+            matchesMask = [[0,0] for _ in range(len(matches))]
+            good_matches = []
             for m,n in matches:
                 if m.distance < 0.7*n.distance:
                     good_matches.append(m)
@@ -74,16 +95,15 @@ for i in kpics.keys():
                 crnr_pts_img1 = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
                 crnr_pts_img2 = cv2.perspectiveTransform(crnr_pts_img1,H)
 
-                img2 = cv2.polylines(img2,[np.int32(dst)],True,(0,255,0),10, cv2.LINE_AA)
+                img2 = cv2.polylines(img2,[np.int32(crnr_pts_img2)],True,(0,255,0),10, cv2.LINE_AA)
                 
                 draw_params = dict(matchColor = (0,255,0),singlePointColor = None,matchesMask = matchesMask, flags = 2)
                 result_img = cv2.drawMatches(img1,kp1,img2,kp2,good_matches,None,**draw_params)
 #                 plt.imshow(result_img),plt.show()
-                plt.imsave(folder + "/" +  "temp_" + i + "_" + j + ".png", result_img)
+                plt.imsave(folder + "/" +  "temp1_" + i + "_" + j + ".png", result_img)
                 print("Feature matching for images pairs {} and {} done".format(i, j))
             else:
                 print ('Not enough matches are found between {} and {}'.format(i, j))
-            good_matches.clear()
     print("Matching features for image {} with all image pairs Done".format(i))
 print("Feature matching done for all images")
 print("End !!")
